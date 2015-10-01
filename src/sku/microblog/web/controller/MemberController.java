6954855cc7b39e132666201da07780cffb328a73
JungDahye <dah9316@gmail.com>
2015-10-01 10:26:28 +0900

    MemberController 작성
    회원가입 및 탈퇴

--- src/sku/microblog/web/controller/MemberController.java
+++ src/sku/microblog/web/controller/MemberController.java
@@ -1,11 +1,19 @@
 package sku.microblog.web.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import sku.microblog.business.domain.Member;
+import sku.microblog.business.service.MemberService;
+import sku.microblog.business.service.MemberServiceImpl;
 import sku.microblog.util.DataDuplicatedException;
 import sku.microblog.util.DataNotFoundException;
 
@@ -52,8 +60,49 @@ public class MemberController extends HttpServlet {
 	 * @throws DataDuplicatedException
 	 */
 	private void registerMember(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DataDuplicatedException  {
-		// TODO Auto-generated method stub
+		// 요청 파라미터를 통해 HTML 등 데이터를 얻어낸다.
+		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
 		
+		//폼 데이터가 유효한 지 검증한다.
+		List<String> errorMsgs = new ArrayList<String>();
+		
+		if(email == null || email.length() == 0){
+			errorMsgs.add("이메일을 입력해주세요.");
+		}
+		
+		if(name == null || name.length() == 0){
+			errorMsgs.add("이름을 입력해주세요.");
+		}
+		
+		if(password == null || password.length() == 0){
+			errorMsgs.add("비밀번호를 입력해주세요.");
+		}
+		
+		//유효하지 않은 데이터가 있으면 에러 페이지를 출력한다.
+		if(!errorMsgs.isEmpty()){
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("회원가입이 실패했습니다.");
+			out.close();
+			return;
+		}
+		
+		//데이터가 유효하면 새로운 회원을 등록하는 처리를 한다. 적절한 데이터를 가진 member 객체를 생성한다.
+		Member member = new Member(email, name, password);
+		
+		//memberservice 객체에 위임하여 회원을 등록한다.
+		MemberService memberService = new MemberServiceImpl();
+		memberService.registerMember(member);
+		
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("회원가입성공^^");
+		out.close();
+		return;
 	}
 
 	/**
@@ -65,7 +114,27 @@ public class MemberController extends HttpServlet {
 	 * @throws DataNotFoundException
 	 */
 	private void deregisterMember(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DataNotFoundException  {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		
+		if(session == null) {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN, "로그인이 필요합니다.");
+			return;
+		}
+		
+		Member member = (Member) session.getAttribute("loginMember");
+		if(member == null) {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN, "로그인이 필요합니다.");
+			return;
+		}
+		
+		MemberService memberService = new MemberServiceImpl();
+		memberService.removeMember(member);
+		
+		session.removeAttribute("loginMember");
+		session.invalidate();
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
 		
 	}
 
